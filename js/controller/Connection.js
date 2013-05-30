bbb7a0c18b7157a6f509561690dd84bef31fc446
gihankarunarathne <gckarunarathne@gmail.com>
2013-05-30 06:52:47 +0530

    Add pass player

--- js/controller/Connection.js
+++ js/controller/Connection.js
@@ -1,6 +1,13 @@
 // script to handle websockets
 var ws = null;
+var server = new function(){
+  userID = 0,
+  URL = "",
+  port = 0
+};
+var passServer = false;
 
+// ========= first form =====================
 $('#connect').click(function() {
   var hostProtocol;
   var hostLocation = 'localhost:8081'
@@ -12,14 +19,13 @@ $('#connect').click(function() {
   }
 
    var URL = "ws://" + hostLocation + '/sangraama-server/org/sangraama/controller/playerservlet';
-  //var URL = "ws://" + hostLocation + '/sangraama-server/org/sangraama/controller/EventHandler';
    
   console.log(URL);
   if ('MozWebSocket' in window) {
     ws = new MozWebSocket(URL);
   } else if ('WebSocket' in window) {
-	  console.log("Add new Player");
-    ws = new WebSocket(URL,["NewPlayer","PassPlayer"]);
+    ws = new WebSocket(URL,"NewPlayer");
+    //ws = new WebSocket(URL,"PassPlayer");
   } else {
     alert('Your browser does not support WebSockets');
     return;
@@ -30,15 +36,82 @@ $('#connect').click(function() {
   };
   ws.onmessage = function(event) {
     var delta = $.evalJSON(event.data);
-    console.log('userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+    console.log('get userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+    if(delta.type != undefined ){
+        console.log('type '+delta.type+' userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+    }else{
+        console.log('type '+delta.type+' userID:'+delta.userID+' dx:' + delta.newServerURL + ' dy:' + delta.newServerPort);
+        server.userID = delta.userID;
+        server.URL = delta.newServerURL;
+        server.port = delta.newServerPort;
+        passServer = true;
+        addMessage('New server '+server.URL+':'+server.port);
+    }
   };
   ws.onclose = function() {
+      isConnect= false;
     addMessage('Offline!');
   };
   ws.onerror = function(event) {
     addMessage('There was an error!');
   };
 });
+// ============ second form ====================
+$('#passconnect').click(function() {
+    console.log('Passconect pressed');
+    var hostProtocol;
+    var hostLocation = server.URL+':'+server.port; //'localhost:8080'
+    var isConnect = false;
+
+    if (window.location.protocol == 'http:') {
+      hostProtocol = 'ws://';
+    } else {
+      hostProtocol = 'wss://';
+    }
+
+     var URL = "ws://" + hostLocation + '/sangraama-server/org/sangraama/controller/playerservlet';
+     
+    console.log(URL);
+    if ('MozWebSocket' in window) {
+      ws = new MozWebSocket(URL);
+    } else if ('WebSocket' in window) {
+      //ws = new WebSocket(URL,"NewPlayer");
+      ws = new WebSocket(URL,"PassPlayer");
+    } else {
+      alert('Your browser does not support WebSockets');
+      return;
+    }
+    
+    ws.onopen = function() {
+      addMessage(' Pass Concection !');
+//      if(isConnect){
+//          var delta = $.evalJSON(event.data);
+//          console.log('userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+//      }else{
+//          var delta = $.evalJSON(event.data);
+//          console.log('userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+//      }
+    };
+    ws.onmessage = function(event) {
+          var delta = $.evalJSON(event.data);
+          console.log('userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+          if(delta.userID > 0){
+              console.log('userID:'+delta.userID+' dx:' + delta.dx + ' dy:' + delta.dy);
+          }else{
+              server.userID = delta.userID;
+              server.URL = delta.newServerURL;
+              server.port = delta.newServerPort;
+              passServer = true;
+              addMessage('New server '+server.URL+':'+server.port);
+          }
+    };
+    ws.onclose = function() {
+      addMessage('Offline !');
+    };
+    ws.onerror = function(event) {
+      addMessage('There was an error!');
+    };
+  });
 
 function addMessage(message) {
   $('#msgArea').append('\n' + message);
