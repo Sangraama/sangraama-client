b13565fe07ab26f7aa1745d0492e385b2dd7c9b2
Gihan Karunarathne <gckarunarathne@gmail.com>
2013-07-12 21:03:23 +0530

    Change size of canvas fitting to current window size

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+<!--
+function viewport(){
+  var e = window, a = 'inner';
+  if ( !( 'innerWidth' in window ) ){
+    a = 'client';
+    e = document.documentElement || document.body;
+  }
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+}
+// -->
